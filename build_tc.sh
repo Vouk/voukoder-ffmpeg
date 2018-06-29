3bcda300bb1381a6426aa87bc41b0f0c3b98fbe4
Daniel Stankewitz <daniel.stankewitz@gmail.com>
2018-06-29 15:03:02 +0200

    Update build_tc.sh

--- build_tc.sh
+++ build_tc.sh
@@ -88,7 +88,7 @@ function compile {
   echo "Compiling '$1' ...";
   cd $SRC/$1
   make clean
-  CC=cl.exe ./configure --prefix=$BUILD $2
+  CC=cl.exe LD=link.exe ./configure --prefix=$BUILD $2
   make -j $CPU_CORES
   make install
 }
