cc9341dc0e5c95dc442243bcd8ed93d486e564ec
Daniel Stankewitz <daniel.stankewitz@gmail.com>
2018-07-04 22:14:19 +0200

    Update build_tc.sh

--- build_tc.sh
+++ build_tc.sh
@@ -82,7 +82,10 @@ elif [ "$STEP" == "libogg" ]; then
 elif [ "$STEP" == "libvorbis" ]; then
   compile libvorbis "--disable-shared"
 elif [ "$STEP" == "libvpx" ]; then
-  compile libvpx "--target=x86_64-win64-vs15 --enable-vp8 --enable-vp9 --disable-shared --disable-examples --disable-tools --disable-docs --enable-vp9-highbitdepth"
+  cd $SRC/$1
+  ./configure --prefix=$BUILD --target=x86_64-win64-vs15 --enable-vp8 --enable-vp9 --disable-shared --disable-examples --disable-tools --disable-docs --enable-vp9-highbitdepth
+  make -j $CPU_CORES
+  make install
 elif [ "$STEP" == "ffmpeg" ]; then
   echo "### Copying NVENC headers ..."
   cd $SRC/ffnvcodec
