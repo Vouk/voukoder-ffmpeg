ab82bda2b119482d030d573d48449bbf352b338d
Daniel Stankewitz <daniel.stankewitz@gmail.com>
2019-08-15 18:38:28 +0200

    Update build_tc.sh

--- build_tc.sh
+++ build_tc.sh
@@ -35,32 +35,32 @@ function compile {
 }
 
 if [ "$STEP" == "svt" ]; then 
+  #
   # HEVC
   cd $SRC/svt-hevc/Build
-  cmake .. -G"Visual Studio 15 2017" -A x64 -DCMAKE_INSTALL_PREFIX=$BUILD -DCMAKE_CONFIGURATION_TYPES="Debug;Release"
-  MSBuild.exe /maxcpucount:$CPU_CORES /property:Configuration="$MSBUILD_CONFIG" /property:ConfigurationType="StaticLibrary" /property:TargetExt=".lib"  Source/Lib/C_DEFAULT/HEVC_C_DEFAULT.vcxproj
-  cp -r ../Source/API $BUILD/include/svt-hevc
-  cp Source/Lib/C_DEFAULT/HEVC_C_DEFAULT.dir/$MSBUILD_CONFIG/HEVC_C_DEFAULT.lib $BUILD/lib/SvtHevcEnc.lib
-  cp SvtHevcEnc.pc $BUILD/lib/pkgconfig/
-  cd $SRC/ffmpeg
-  git apply ../svt-hevc/ffmpeg_plugin/*.patch
+  cmake .. -G"Visual Studio 15 2017" -A x64 -DCMAKE_INSTALL_PREFIX=$BUILD -DCMAKE_CONFIGURATION_TYPES=$MSBUILD_CONFIG
+  MSBuild.exe /maxcpucount:$CPU_CORES /property:Configuration=$MSBUILD_CONFIG /property:ConfigurationType=StaticLibrary /property:TargetExt=.lib  svt-hevc.sln  
+  # install
+  cp -r ../Source/API $BUILD/include/svt-hevc ; cp ../Bin/$MSBUILD_CONFIG/SvtHevcEnc.lib $BUILD/lib/ ; cp SvtHevcEnc.pc $BUILD/lib/pkgconfig/
+  #
   # AV1
   cd $SRC/svt-av1/Build
-  cmake .. -G"Visual Studio 15 2017" -A x64 -DCMAKE_INSTALL_PREFIX=$BUILD -DCMAKE_CONFIGURATION_TYPES="Debug;Release"
-  MSBuild.exe /maxcpucount:$CPU_CORES /property:Configuration="$MSBUILD_CONFIG" /property:ConfigurationType="StaticLibrary" /property:TargetExt=".lib" Source/Lib/Encoder/SvtAv1Enc.vcxproj
-  cp -r ../Source/API $BUILD/include/svt-av1
-  cp ../Bin/Release/$MSBUILD_CONFIG/SvtAv1Enc.lib $BUILD/lib/
-  cp SvtAv1Enc.pc $BUILD/lib/pkgconfig/
-  cd $SRC/ffmpeg
-  git apply ../svt-av1/ffmpeg_plugin/0001-Add-ability-for-ffmpeg-to-run-svt-av1-with-svt-hevc.patch
+  cmake .. -G"Visual Studio 15 2017" -A x64 -DCMAKE_INSTALL_PREFIX=$BUILD -DCMAKE_CONFIGURATION_TYPES=$MSBUILD_CONFIG
+  MSBuild.exe /maxcpucount:$CPU_CORES /property:Configuration=$MSBUILD_CONFIG /property:ConfigurationType=StaticLibrary /property:TargetExt=.lib svt-av1.sln
+  # install
+  cp -r ../Source/API $BUILD/include/svt-av1 ; cp ../Bin/Release/$MSBUILD_CONFIG/SvtAv1Enc.lib $BUILD/lib/ ; cp SvtAv1Enc.pc $BUILD/lib/pkgconfig/
+  #
   # VP9
   cd $SRC/svt-vp9/Build
-  cmake .. -G"Visual Studio 15 2017" -A x64 -DCMAKE_INSTALL_PREFIX=$BUILD -DCMAKE_CONFIGURATION_TYPES="Debug;Release"
-  MSBuild.exe /maxcpucount:$CPU_CORES /property:Configuration="$MSBUILD_CONFIG" /property:ConfigurationType="StaticLibrary" /property:TargetExt=".lib" Source/Lib/C_DEFAULT/C_DEFAULT.vcxproj
-  cp -r ../Source/API $BUILD/include/svt-vp9
-  cp lib/$MSBUILD_CONFIG/C_DEFAULT.lib $BUILD/lib/SvtVp9Enc.lib
-  cp SvtVp9Enc.pc $BUILD/lib/pkgconfig/
+  cmake .. -G"Visual Studio 15 2017" -A x64 -DCMAKE_INSTALL_PREFIX=$BUILD -DCMAKE_CONFIGURATION_TYPES=$MSBUILD_CONFIG
+  MSBuild.exe /maxcpucount:$CPU_CORES /property:Configuration=$MSBUILD_CONFIG /property:ConfigurationType=StaticLibrary /property:TargetExt=".lib" svt-vp9.sln
+  # install
+  cp -r ../Source/API $BUILD/include/svt-vp9 ; cp ../Bin/$MSBUILD_CONFIG/SvtVp9Enc.lib $BUILD/lib/ ; cp SvtVp9Enc.pc $BUILD/lib/pkgconfig/
+  #
+  # PATCHES
   cd $SRC/ffmpeg
+  git apply ../svt-hevc/ffmpeg_plugin/0001-lavc-svt_hevc-add-libsvt-hevc-encoder-wrapper.patch
+  git apply ../svt-av1/ffmpeg_plugin/0001-Add-ability-for-ffmpeg-to-run-svt-av1-with-svt-hevc.patch
   git apply ../svt-vp9/ffmpeg_plugin/0001-Add-ability-for-ffmpeg-to-run-svt-vp9-with-svt-hevc-av1.patch
 elif [ "$STEP" == "libmfx" ]; then
   cd $SRC/libmfx
