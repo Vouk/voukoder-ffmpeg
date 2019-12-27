b0a99a749bdf72b4f147cf115e901ab2e89f4c1f
Daniel Stankewitz <daniel.stankewitz@gmail.com>
2019-12-27 12:24:25 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -39,7 +39,7 @@ function build_amf {
 function build_mfx {
   git clone https://github.com/lu-zero/mfx_dispatch.git $SRC/libmfx
   cd $SRC/libmfx
-  git checkout 1.25
+  git checkout c200d833e25a91e3e49d69890dac1ffa3486cbe9
   if [[ ! -f "configure" ]]; then
       autoreconf -fiv || exit 1
   fi
