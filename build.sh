71f4d34d5d2a298330f8d95a56f89d2b0d9846fb
Daniel Stankewitz <daniel.stankewitz@gmail.com>
2019-12-28 10:03:10 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -218,18 +218,18 @@ git clone git://source.ffmpeg.org/ffmpeg.git $SRC/ffmpeg
 
 build_nvenc
 build_amf
-build_mfx
-build_svt
+#build_mfx
+#build_svt
 build_ogg
 build_vorbis
-build_snappy
-build_libvpx
+#build_snappy
+#build_libvpx
 build_libfdkaac
 build_lame
 build_zimg
-build_x264
+#build_x264
 build_opus
-build_x265
+#build_x265
 
 cd $SRC/ffmpeg
 PKG_CONFIG_PATH=$BUILD/lib/pkgconfig:$PKG_CONFIG_PATH ./configure --toolchain=msvc --extra-cflags="$CFLAGS -I$BUILD/include" --extra-ldflags="-LIBPATH:$BUILD/lib" --prefix=$BUILD --pkg-config-flags="--static" --disable-doc --disable-shared --enable-static --enable-runtime-cpudetect --disable-devices --disable-demuxers --disable-decoders --disable-network --enable-w32threads --enable-gpl $COMPONENTS
