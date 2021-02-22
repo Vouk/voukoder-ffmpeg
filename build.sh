5b0edc68885799cb984ced1f7ae3d5e92a1421ce
Daniel Stankewitz <daniel.stankewitz@gmail.com>
2021-02-22 09:52:30 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -80,7 +80,7 @@ function build_svt {
   cd $SRC/svt-av1/Build
   cmake .. -G"Visual Studio 15 2017" -A x64 -DCMAKE_INSTALL_PREFIX=$BUILD -DCMAKE_CONFIGURATION_TYPES=$MSBUILD_CONFIG
   MSBuild.exe /maxcpucount:$NUMBER_OF_PROCESSORS /property:Configuration=$MSBUILD_CONFIG /property:ConfigurationType=StaticLibrary /property:TargetExt=.lib Source/Lib/Encoder/SvtAv1Enc.vcxproj
-  sed -i 's/C:/\/c/g' SvtAv1Enc.pc
+  #sed -i 's/C:/\/c/g' SvtAv1Enc.pc
   cp -r ../Source/API $BUILD/include/svt-av1 ; cp ../Bin/$MSBUILD_CONFIG/SvtAv1Enc.lib $BUILD/lib/ ; cp SvtAv1Enc.pc $BUILD/lib/pkgconfig/
   add_comp libsvtav1
   #
@@ -250,20 +250,20 @@ SRC=`realpath src`
 #git clone -b release/4.3 git://source.ffmpeg.org/ffmpeg.git $SRC/ffmpeg
 git clone git://source.ffmpeg.org/ffmpeg.git $SRC/ffmpeg
 
-#build_nvenc
-#build_amf
-#build_mfx
-build_svt
-#build_ogg
-#build_vorbis
-#build_snappy
-#build_libvpx
-#build_libfdkaac
-#build_lame
-#build_zimg
-#build_x264
-#build_opus
-#build_x265
+build_nvenc
+build_amf
+build_mfx
+#build_svt
+build_ogg
+build_vorbis
+build_snappy
+build_libvpx
+build_libfdkaac
+build_lame
+build_zimg
+build_x264
+build_opus
+build_x265
 #build_aom
 #build_libass
 
@@ -280,7 +280,7 @@ for file in *.a; do
 done
 
 # clean up
-#rm -rf $BUILD/lib/pkgconfig $BUILD/lib/fdk-aac.lib $BUILD/lib/*.la
+rm -rf $BUILD/lib/pkgconfig $BUILD/lib/fdk-aac.lib $BUILD/lib/*.la
   
 # Create archives
 cd $BUILD
