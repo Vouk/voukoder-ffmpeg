3f53de0b1e2692774bc163d9d4fa7c5f3b84cb12
Daniel Stankewitz <daniel.stankewitz@gmail.com>
2021-06-21 08:10:23 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -144,7 +144,7 @@ function build_libfdkaac {
   build fdk-aac "--disable-static --disable-shared"
   add_comp libfdk-aac
   cd $SRC/ffmpeg
-  patch -N -p1 -i ../../patches/0003-dynamic-loading-of-shared-fdk-aac-library.patch
+  patch -N -p1 -i ../../patches/0003-dynamic-loading-of-shared-fdk-aac-library-4.4.patch
 }
 
 function build_lame {
